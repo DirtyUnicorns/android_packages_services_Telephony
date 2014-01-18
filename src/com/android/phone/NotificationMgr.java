7aa9ce6386daf24a7484edf5f96d2093d7828a06
garwedgess <garethwilliams21@gmail.com>
2014-01-18 22:15:18 +0530

    [2/5] Breathing Notifications
    Updated for Marshmallow by DU
    
    Change-Id: I7f78d37561708362e0002edbdbbec63d0279db3e

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -33,6 +33,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract.PhoneLookup;
+import android.provider.Settings;
 import android.telecom.PhoneAccount;
 import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
@@ -230,7 +231,13 @@ public class NotificationMgr {
                 return;
             }
 
-            int resId = android.R.drawable.stat_notify_voicemail;
+            int resId;
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1) {
+                resId = R.drawable.stat_notify_voicemail_breath;
+            } else {
+                resId = android.R.drawable.stat_notify_voicemail;
+            }
 
             // This Notification can get a lot fancier once we have more
             // information about the current voicemail messages.
