13458cd34485e8f41d4d25f3c625fb57173ef847
Alex Cruz <mazdarider23@gmail.com>
2014-08-06 22:24:27 -0400

    Revert "TeloRadio [3/3]"
    This works sometimes on gsm devices and doesn't work at all on cdma devices
    
    This reverts commit ba1882a352f4b587ca17cea884242afb20d9dcae.
    
    Change-Id: I26c02aab13928cc9d648fe3901c46aee6d3d0fe7

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -34,6 +34,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.os.UserHandle;
 import android.telephony.NeighboringCellInfo;
 import android.telephony.CellInfo;
@@ -75,6 +76,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
     private static final int CMD_ANSWER_RINGING_CALL = 4;
     private static final int CMD_END_CALL = 5;  // not used yet
     private static final int CMD_SILENCE_RINGER = 6;
+    private static final int CMD_TOGGLE_LTE = 7; // not used yet
+    private static final int CMD_TOGGLE_2G = 8; // used in screen action implementation
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -320,6 +323,19 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
         Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mApp.startActivity(intent);
+    }
+
+    public void toggle2G(boolean on) {
+        int network = -1;
+        if (on) {
+            network = PhoneConstants.NT_MODE_GSM_ONLY;
+        } else {
+            network = PhoneConstants.NT_MODE_WCDMA_PREF;
+        }
+        mPhone.setPreferredNetworkType(network,
+                mMainThreadHandler.obtainMessage(CMD_TOGGLE_2G));
+        Settings.Secure.putInt(mApp.getContentResolver(),
+                Settings.Global.PREFERRED_NETWORK_MODE, network);
     }
 
     private boolean showCallScreenInternal(boolean specifyInitialDialpadState,
