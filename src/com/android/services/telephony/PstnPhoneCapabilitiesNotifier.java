b503eaccee6ae3736620735af214e4cc2d5958c8
Sandeep Kunta <skunta@codeaurora.org>
2015-03-17 16:49:18 +0530

    Align the setIcon usage as per new interface
    Align the setIcon usage as per new interface
    
    Change-Id: I926223c7b1dcbc056b17ecd29bd47bfc370bc024

--- src/com/android/services/telephony/PstnPhoneCapabilitiesNotifier.java
+++ src/com/android/services/telephony/PstnPhoneCapabilitiesNotifier.java
@@ -141,7 +141,7 @@ final class PstnPhoneCapabilitiesNotifier {
                     .setAddress(oldPhoneAccount.getAddress())
                     .setSubscriptionAddress(oldPhoneAccount.getSubscriptionAddress())
                     .setCapabilities(capabilites)
-                    .setIcon(mPhoneProxy.getContext(), oldPhoneAccount.getIconResId())
+                    .setIcon(oldPhoneAccount.getIconBitmap())
                     .setShortDescription(oldPhoneAccount.getShortDescription())
                     .setSupportedUriSchemes(oldPhoneAccount.getSupportedUriSchemes())
                     .build();
