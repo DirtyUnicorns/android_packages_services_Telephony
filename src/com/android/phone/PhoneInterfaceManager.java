5b6b3915bbe43015f930e5c714e57e8cf66d8ea7
Sandeep Gutta <sangutta@codeaurora.org>
2015-04-09 12:20:06 +0530

    MSIM: Multiple multi sim specific fixes
    - Fix to send sub specific cell information for
    method getAllCellInfoUsingSubId()
    
    -Fix to handle PIM MMI requests on the subId
    provided by clients.
    
    Change-Id: I67d9346131fac7cef73fb5c4d41ddd887093da70

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -208,7 +208,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             switch (msg.what) {
                 case CMD_HANDLE_PIN_MMI:
                     request = (MainThreadRequest) msg.obj;
-                    request.result = mPhone.handlePinMmi((String) request.argument);
+                    request.result =
+                            getPhoneFromRequest(request).handlePinMmi((String) request.argument);
                     // Wake up the requesting thread
                     synchronized (request) {
                         request.notifyAll();
@@ -251,7 +252,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                     if (phoneType == PhoneConstants.PHONE_TYPE_CDMA) {
                         // CDMA: If the user presses the Power button we treat it as
                         // ending the complete call session
-                        hungUp = PhoneUtils.hangupRingingAndActive(mPhone);
+                        hungUp = PhoneUtils.hangupRingingAndActive(getPhone(end_subId));
                     } else if (phoneType == PhoneConstants.PHONE_TYPE_GSM) {
                         // GSM: End the call as per the Phone state
                         hungUp = PhoneUtils.hangup(mCM);
@@ -658,8 +659,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                     request = (MainThreadRequest) msg.obj;
                     onCompleted = obtainMessage(EVENT_SET_VOICEMAIL_NUMBER_DONE, request);
                     Pair<String, String> tagNum = (Pair<String, String>) request.argument;
-                    Phone phone = (request.subId == null) ? mPhone : getPhone(request.subId);
-                    phone.setVoiceMailNumber(tagNum.first, tagNum.second, onCompleted);
+                    getPhoneFromRequest(request).setVoiceMailNumber(tagNum.first, tagNum.second,
+                            onCompleted);
                     break;
 
                 case EVENT_SET_VOICEMAIL_NUMBER_DONE:
@@ -780,6 +781,10 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         if (DBG) log("publish: " + this);
 
         ServiceManager.addService("phone", this);
+    }
+
+    private Phone getPhoneFromRequest(MainThreadRequest request) {
+        return (request.subId == null) ? mPhone : getPhone(request.subId);
     }
 
     // returns phone associated with the subId.
@@ -1221,7 +1226,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
 
     public boolean handlePinMmiForSubscriber(int subId, String dialString) {
         enforceModifyPermission();
-        return (Boolean) sendRequest(CMD_HANDLE_PIN_MMI, dialString, subId);
+        return (Boolean) sendRequest(CMD_HANDLE_PIN_MMI, dialString, new Integer(subId));
     }
 
     public int getCallState() {
@@ -1326,14 +1331,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         }
     }
 
-
     @Override
     public List<CellInfo> getAllCellInfo() {
-        return getAllCellInfoUsingSubId(getDefaultSubscription());
-    }
-
-    @Override
-    public List<CellInfo> getAllCellInfoUsingSubId(int subId) {
         try {
             mApp.enforceCallingOrSelfPermission(
                 android.Manifest.permission.ACCESS_FINE_LOCATION, null);
@@ -1354,6 +1353,28 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             return cellInfos;
         } else {
             if (DBG_LOC) log("getAllCellInfo: suppress non-active user");
+            return null;
+        }
+    }
+
+    @Override
+    public List<CellInfo> getAllCellInfoUsingSubId(int subId) {
+        try {
+            mApp.enforceCallingOrSelfPermission(
+                android.Manifest.permission.ACCESS_FINE_LOCATION, null);
+        } catch (SecurityException e) {
+            // If we have ACCESS_FINE_LOCATION permission, skip the check for ACCESS_COARSE_LOCATION
+            // A failure should throw the SecurityException from ACCESS_COARSE_LOCATION since this
+            // is the weaker precondition
+            mApp.enforceCallingOrSelfPermission(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION, null);
+        }
+
+        if (checkIfCallerIsSelfOrForegroundUser()) {
+            if (DBG_LOC) log("getAllCellInfoUsingSubId: is active user");
+            return getPhone(subId).getAllCellInfo();
+        } else {
+            if (DBG_LOC) log("getAllCellInfoUsingSubId: suppress non-active user");
             return null;
         }
     }
@@ -1541,7 +1562,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      */
     public String getCdmaMdn(int subId) {
         enforceModifyPermissionOrCarrierPrivilege();
-        if (mPhone.getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
+        if (getPhone(subId).getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
             return getPhone(subId).getLine1Number();
         } else {
             return null;
@@ -1553,7 +1574,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      */
     public String getCdmaMin(int subId) {
         enforceModifyPermissionOrCarrierPrivilege();
-        if (mPhone.getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
+        if (getPhone(subId).getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
             return getPhone(subId).getCdmaMin();
         } else {
             return null;
@@ -1572,7 +1593,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      */
     @Override
     public boolean setVoiceMailNumber(int subId, String alphaTag, String number) {
-        enforceModifyPermissionOrCarrierPrivilege();
+        enforceCarrierPrivilege();
         Boolean success = (Boolean) sendRequest(CMD_SET_VOICEMAIL_NUMBER,
                 new Pair<String, String>(alphaTag, number), new Integer(subId));
         return success;
